c3c650cc0b7fdf5724d26a0e257ac9c982f0def6
Shelvacu <shelvacu@gmail.com>
2019-08-31 21:00:03 -0700

    Maud joins the party!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro, type_ascription)]
 
 #[macro_use] extern crate rocket;
+extern crate maud;
+
+use maud::{html, DOCTYPE, Markup};
 
 #[get("/math")]
-fn index() -> &'static str {
-    "2+2=4"
+fn index() -> Markup {
+    let inner_str = "2+2=4";
+    html! {
+        (DOCTYPE)
+        head {
+            title { "Some awesome math" }
+            meta charset="utf-8";
+            meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no";
+        }
+        body {
+            p {
+                "Hey look at this awesome math" .math { (inner_str) }
+            }
+        }
+    }       
 }
 
 fn main() {
