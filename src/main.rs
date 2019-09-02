530fd97832d12d5d137e0435355732e3aa80f449
Shelvacu <shelvacu@gmail.com>
2019-09-01 23:57:24 -0700

    make stupid simple base64 conversion, no urlsafe support

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 
 #[macro_use] extern crate rocket;
 extern crate maud;
+extern crate base64;
 
 use maud::{html, DOCTYPE, Markup};
 
 #[get("/math")]
-fn index() -> Markup {
+fn math() -> Markup {
     let inner_str = "2+2=4";
     html! {
         (DOCTYPE)
@@ -23,6 +24,39 @@ fn index() -> Markup {
     }       
 }
 
+#[post("/base64/enc", data="<input>")]
+fn base64_enc(input: Vec<u8>) -> String {
+    base64::encode(&input)
+}
+
+/*fn base64_dec(input: String) -> Result<Vec<u8>, {
+    base64::encode(&input)
+}*/
+
+#[post("/base64/dec", data="<input>")]
+fn base64_dec(input: String) -> Result<Vec<u8>,&'static str> {
+    base64_dec_str(input)
+}
+
+#[get("/base64/dec/<input>")]
+fn base64_dec_str(input: String) -> Result<Vec<u8>,&'static str> {
+    base64::decode(&input).map_err(|_| "Could not decode string")
+}
+
+#[get("/base64/enc/<input>")]
+fn base64_enc_str(input: String) -> String {
+    base64::encode(&input)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount(
+        "/",
+        routes![
+            math,
+            base64_dec_str,
+            base64_enc_str,
+            base64_enc,
+            base64_dec
+        ]
+    ).launch();
 }
